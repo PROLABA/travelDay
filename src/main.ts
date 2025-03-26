5edc5b38262e8557112ecda88321fefd06dc5158
Азат Фарваев <afarvaev4@gmail.com>
2025-03-26 18:02:08 +0300

    слайдер главной страницы

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { mainSwiper } from "./scripts/mainSwiper";
 import { searchAction } from "./scripts/searchAction/searchAction";
 import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
 
 document.addEventListener("DOMContentLoaded", () => {
   searchAction();
   swiperDiscount();
+  mainSwiper();
 });
---
+++ src/scripts/mainSwiper/index.ts
@@ -0,0 +1,21 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export const mainSwiper = () => {
+  const swiper = new Swiper(".main-swiper", {
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: ".main-swiper-button-next ",
+      prevEl: ".main-swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    spaceBetween: 10,
+    slidesPerView: 1,
+  });
+};
