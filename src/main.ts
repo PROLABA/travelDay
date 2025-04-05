da1a8ebfbe76c6bb49d886e104ac58c68ec49547
Minichad <lexa00911@gmail.com>
2025-04-05 23:06:19 +0300

    Созданы страницы 'О компании' и 'Вопросы и ответы'

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { sliderCard } from "./scripts/cardSlider";
-import { mainSwiper } from "./scripts/mainSwiper";
-import { mobileSearchAction } from "./scripts/mobileSearchAction";
-import { searchAction } from "./scripts/searchAction/searchAction";
-import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
+  import { sliderCard } from "./scripts/cardSlider";
+  import { mainSwiper } from "./scripts/mainSwiper";
+  import { mobileSearchAction } from "./scripts/mobileSearchAction";
+  import { searchAction } from "./scripts/searchAction/searchAction";
+  import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
+  import { faqAccordion } from "./scripts/faqAccordion";
 
-document.addEventListener("DOMContentLoaded", () => {
-  searchAction();
-  swiperDiscount();
-  mainSwiper();
-  sliderCard();
-  mobileSearchAction();
-});
+  document.addEventListener("DOMContentLoaded", () => {
+    searchAction();
+    swiperDiscount();
+    mainSwiper();
+    sliderCard();
+    mobileSearchAction();
+    faqAccordion();
+  });
