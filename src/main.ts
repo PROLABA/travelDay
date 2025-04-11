6bc8c905d0e79a2b10ad58f1649e708fc9cc9abd
Minichad <lexa00911@gmail.com>
2025-04-11 23:07:13 +0300

    Микро фикс

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-  import { sliderCard } from "./scripts/cardSlider";
-  import { mainSwiper } from "./scripts/mainSwiper";
-  import { mobileSearchAction } from "./scripts/mobileSearchAction";
-  import { searchAction } from "./scripts/searchAction/searchAction";
-  import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
-  import { faqAccordion } from "./scripts/faqAccordion";
-  import { ContactForm } from "./scripts/contactsForm";
+import { sliderCard } from "./scripts/cardSlider";
+import { mainSwiper } from "./scripts/mainSwiper";
+import { mobileSearchAction } from "./scripts/mobileSearchAction";
+import { searchAction } from "./scripts/searchAction/searchAction";
+import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
+import { faqAccordion } from "./scripts/faqAccordion";
+import { ContactForm } from "./scripts/contactsForm";
 
-  document.addEventListener("DOMContentLoaded", () => {
-    searchAction();
-    swiperDiscount();
-    mainSwiper();
-    sliderCard();
-    mobileSearchAction();
-    faqAccordion();
-    new ContactForm();
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  searchAction();
+  swiperDiscount();
+  mainSwiper();
+  sliderCard();
+  mobileSearchAction();
+  faqAccordion();
+  new ContactForm();
+});
