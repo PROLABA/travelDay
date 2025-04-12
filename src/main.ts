b80306afaafb0c08887795697e048ae69fde3288
Minichad <lexa00911@gmail.com>
2025-04-12 19:39:34 +0300

    Добавление видео на страницу

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { mobileSearchAction } from "./scripts/mobileSearchAction";
 import { searchAction } from "./scripts/searchAction/searchAction";
 import { swiperDiscount } from "./scripts/swiperDiscount/swiperDiscount";
 import { faqAccordion } from "./scripts/faqAccordion";
+import { initVideoPlayers } from "./scripts/videoPlayer";
 import { ContactForm } from "./scripts/contactsForm";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,5 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
   sliderCard();
   mobileSearchAction();
   faqAccordion();
+  initVideoPlayers();
   new ContactForm();
 });
