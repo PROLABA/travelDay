749ef7fbd41f6c86f41fbae95b5668618bb81368
ssslava <aslavadoma@tuta.io>
2025-04-23 18:57:49 +0700

    feat: add bad adaptive

--- src/scripts/modules/datepicker.ts
+++ src/scripts/modules/datepicker.ts
@@ -1,50 +1,52 @@
 const initCustomDatepicker = () => {
+    const monthNames = [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь'
+    ];
+    const dayNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const dayNamesMin = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
     $(function () {
         const $datepicker = $('#datepicker');
 
         const isMobile = window.innerWidth < 768;
 
-        if (isMobile) {
-            // initMobileDatepicker();
-        } else {
-            initDesktopDatepicker();
-        }
+        // if (isMobile) {
+        //     initMobileDatepicker();
+        // } else {
+        // }
+        initDesktopDatepicker();
 
         function initDesktopDatepicker() {
-            let clickCount = 0;
-            let startDate = null;
-            let endDate = null;
-
             ($datepicker as any).daterangepicker({
                 datepickerOptions: {
-                    // showOn: 'button',
-                    numberOfMonths: 1,
+					showCurrentAtPos: 0,
+                    numberOfMonths: isMobile ? 12 : 1,
                     minDate: 0,
+                    // maxDate: isMobile ? 12 : null,
                     maxDate: null,
+                    showOn: 'focus',
                     firstDay: 1,
-                    monthNames: [
-                        'Январь',
-                        'Февраль',
-                        'Март',
-                        'Апрель',
-                        'Май',
-                        'Июнь',
-                        'Июль',
-                        'Август',
-                        'Сентябрь',
-                        'Октябрь',
-                        'Ноябрь',
-                        'Декабрь'
-                    ],
-                    dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-                    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+                    monthNames: monthNames,
+                    dayNames: dayNames,
+                    dayNamesMin: dayNamesMin
                 },
 
                 dateFormat: 'dd.mm',
                 initialText: 'Дата',
                 applyButtonText: 'Применить',
-                clearButtonText: '',
+                clearButtonText: isMobile ? 'Сбросить' : '',
                 cancelButtonText: '',
+				autoFitCalendars: isMobile ? false : true,
                 presetRanges: [
                     {
                         text: 'Сегодня',
@@ -79,56 +81,32 @@ const initCustomDatepicker = () => {
                     setTimeout(() => insertCustomButtons(), 50);
                 },
                 icon: 'custom-date-icon'
-                // change: function (e) {
-                //     console.dir(e);
-                //     console.dir('dsada');
-                // }
             });
         }
 
-        function initMobileDatepicker() {
-            $datepicker.daterangepicker({
-                minDate: 0,
-                numberOfMonths: 12,
-                stepMonths: 2,
-                showCurrentAtPos: 0,
-                showOn: 'focus',
-                beforeShow: function (input, inst) {
-                    setTimeout(() => {
-                        insertCustomButtons(inst.dpDiv);
-                        // setupScrollHandler(inst.dpDiv);
-                    }, 10);
-                }
-            });
-        }
-
-        // $datepicker.datepicker({
-        //     numberOfMonths: 2,
-        //     showCurrentAtPos: 0,
-        //     stepMonths: 2,
-        //     minDate: 0,
-        //     showOn: 'button',
-        //     beforeShow: function (input, inst) {
-        //         setTimeout(function () {
-        //             insertCustomButtons(inst.dpDiv);
-        //             setupScrollHandler(inst.dpDiv);
-        //         }, 50);
-        //     }
-        // });
+        // function initMobileDatepicker() {
+        //     $datepicker.daterangepicker({
+        //         datepickerOptions: {
+        //             minDate: 0,
+        //             numberOfMonths: 12,
+        //             // stepMonths: 1,
+        //             showCurrentAtPos: 0,
+        //             showOn: 'focus'
+        //         },
+        //         open: function () {
+        //             setTimeout(() => insertCustomButtons(), 50);
+        //         }
+        //     });
+        // }
 
         $('#date-button').click(function (e) {
             e.preventDefault();
-            // вручную удаляем классы
             $datepicker.daterangepicker('open');
         });
 
         $('#custom-date-clear').click(e => {
             e.preventDefault();
             $datepicker.daterangepicker('clearRange');
-        });
-        $('#date-button-clear').click(e => {
-            e.preventDefault();
-            $('#custom-date-clear').click();
         });
 
         function insertCustomButtons() {
@@ -140,7 +118,6 @@ const initCustomDatepicker = () => {
 
             const title = $wrapper.data('title');
             const descr = $wrapper.data('descr');
-            // const buttons = JSON.parse($wrapper.attr('data-buttons') || '[]');
 
             const $btnContainer = $('<div class="dp-custom-header"></div>');
 
@@ -154,22 +131,6 @@ const initCustomDatepicker = () => {
 
             $modalBodyContent.prepend($btnContainer);
         }
-
-        // function setupScrollHandler($dpDiv) {
-        //     $dpDiv.off('scroll').on('scroll', function () {
-        //         const scrollBottom = $dpDiv[0].scrollHeight - $dpDiv.scrollTop() - $dpDiv.outerHeight();
-
-        //         // Если прокручено до низа
-        //         if (scrollBottom < 10) {
-        //             const currentDate = $datepicker.daterangepicker('getDate') || new Date();
-
-        //             // Сдвигаем на 2 месяца вперёд
-        //             currentDate.setMonth(currentDate.getMonth() + 2);
-        //             $datepicker.daterangepicker('setDate', currentDate);
-        //             $datepicker.daterangepicker('refresh');
-        //         }
-        //     });
-        // }
     });
 };
 export default initCustomDatepicker;
